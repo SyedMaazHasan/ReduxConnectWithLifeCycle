9142ec0a1d57dac893cc6792895218637b772276
Maaz <syed_maaz_hasan@yahoo.com>
2020-01-11 18:17:32 +0530

    added image

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,28 +1,30 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { connectWithLifecycle } from "react-lifecycle-component";
+import logo from "./hierarchy.jpg";
 
 class Counter extends Component {
   componentWillMount() {
-    console.log("componentWillMount inside component");
+    console.log("componentWillMount inside component --before render");
   }
 
   componentDidMount() {
-    console.log("componentDidMount inside component");
+    console.log("componentDidMount inside component --after render");
   }
   shouldComponentUpdate() {
     //console.log("component should update in A");
     return true;
   }
   componentWillUpdate() {
-    console.log("componentWillUpdate inside component");
+    console.log("componentWillUpdate inside component --before render");
   }
   componentDidUpdate() {
-    console.log("componentDidUpdate inside component");
+    console.log("componentDidUpdate inside component --after render");
   }
   render() {
     return (
       <div>
+        <img src={logo} width="500" height="300" />
         <h1> Counter </h1>
         <h1> {this.props.count} </h1>
         <button onClick={this.props.handleIncrease}> Increase </button>
@@ -41,16 +43,24 @@ function mapStateToProps(state) {
 function mapDispatchToProps(dispatch) {
   return {
     componentWillMount: () => {
-      console.log("componentwillmount inside mapDispatchToProps");
+      console.log("*******MOUNTING PHASE*********");
+      console.log(
+        "componentWillmount inside mapDispatchToProps --before render"
+      );
     },
     componentDidMount: () => {
-      console.log("componentdidmount inside mapDispatchToProps");
+      console.log("componentDidmount inside mapDispatchToProps --after render");
     },
     componentWillUpdate: () => {
-      console.log("componentdidmount inside mapDispatchToProps");
+      console.log("*******UPDATION PHASE*********");
+      console.log(
+        "componentWillUpdate inside mapDispatchToProps --before render"
+      );
     },
     componentDidUpdate: () => {
-      console.log("componentdidmount inside mapDispatchToProps");
+      console.log(
+        "componentDidUpdate inside mapDispatchToProps --after render"
+      );
     },
     handleIncrease: () => dispatch({ type: "INCREMENT" }),
     handleDecrease: () => dispatch({ type: "DECREMENT" })
